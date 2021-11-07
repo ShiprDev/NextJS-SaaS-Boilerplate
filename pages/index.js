a98472031d32f68226cc4b98529f390f0e0d41cf
yash desai <yhdelectric@gmail.com>
2021-11-07 11:20:40 +0530

    :lipstick: Add Header on Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/landing/Header'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <Header/>
       
       </main>
 
